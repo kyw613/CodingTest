02d74e18cad92b7b5d7c5af4eb931b4848ec3848
kyw613 <105292383+kyw613@users.noreply.github.com>
2024-10-17 18:54:54 +0900

    [Silver III] Title: 두 수의 합, Time: 84 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,16 +1,21 @@
 N = int(input())
 A = list(map(int, input().split()))
 X = int(input())
+
 A.sort()
 cnt = 0
-total = 0
-# 이걸 정렬했으니까 end를 1~N까지 하고 그때 start하면 되네
-for s in range(N):
-    total = A[s]
-    for j in range(s+1,N):
-        if total + A[j] == X:
-            cnt += 1
-        elif total + A[j] > X:
-            break
-print(cnt)
+start, end = 0, N - 1
+
+while start < end:
+    total = A[start] + A[end]
     
+    if total == X:
+        cnt += 1
+        start += 1
+        end -= 1
+    elif total < X:
+        start += 1  # 합이 X보다 작으면 start를 증가시켜 더 큰 값을 탐색
+    else:
+        end -= 1  # 합이 X보다 크면 end를 감소시켜 더 작은 값을 탐색
+
+print(cnt)
