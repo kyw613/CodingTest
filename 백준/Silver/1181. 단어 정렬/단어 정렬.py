ed578075468047accc92464d8ad1a46c5c76022b
kyw613 <105292383+kyw613@users.noreply.github.com>
2024-10-29 14:33:46 +0900

    [Silver V] Title: 단어 정렬, Time: 556 ms, Memory: 34756 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -3,13 +3,14 @@
 #단, 중복된 단어는 하나만 남기고 제거해야 한다.
 N = int(input())
 
-w_i = []
+w_i = {}
 for _ in range(N):
     a = str(input())
     if not a in w_i:
-        w_i.append(a)
+        w_i[a] = True
 
 
-w_i = sorted(w_i,key=lambda x:(len(x),x))
+w_i = sorted(w_i.keys(),key=lambda x:(len(x),x))
+
 for i in range(len(w_i)):
     print(w_i[i])
