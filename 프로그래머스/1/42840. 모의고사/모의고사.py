52702948fa9d2f343c923893c6663f461a6da2e0
kyw613 <105292383+kyw613@users.noreply.github.com>
2024-12-23 14:42:47 +0900

    [level 1] Title: 모의고사, Time: 2.50 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,24 +1,27 @@
 def solution(answers):
-    # 수포자들의 패턴 정의
-    pattern1 = [1, 2, 3, 4, 5]
-    pattern2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    
-    score = [0, 0, 0]  # 수포자 1, 2, 3의 점수
-    
-    for i in range(len(answers)):
-        if answers[i] == pattern1[i % len(pattern1)]:
-            score[0] += 1
-        if answers[i] == pattern2[i % len(pattern2)]:
-            score[1] += 1
-        if answers[i] == pattern3[i % len(pattern3)]:
-            score[2] += 1
-    
-    # 가장 높은 점수를 받은 수포자
-    max_score = max(score)
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
     result = []
-    for i in range(3):
-        if score[i] == max_score:
-            result.append(i + 1)  
-    
+    total1 = 0
+    total2 = 0
+    total3 = 0
+    for k in range(len(answers)):
+        if answers[k] == pattern1[k%5]:
+            total1 += 1
+        if answers[k] == pattern2[k%8]:
+            total2 += 1
+        if answers[k] == pattern3[k%10]:
+            total3 += 1
+    max_val = max(total1,total2,total3)
+    if total1 == max_val:
+        result.append(1)
+    if total2 == max_val:
+        result.append(2)
+    if total3 == max_val:
+        result.append(3)
     return result
+    
+    
+                
+            
