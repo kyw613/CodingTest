aa0638cd9d43dc4fccda9c2980712417a72e26e4
kyw613 <105292383+kyw613@users.noreply.github.com>
2024-12-23 18:35:38 +0900

    [level 1] Title: 대충 만든 자판, Time: 338.48 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,27 +1,27 @@
 def solution(keymap, targets):
-    # 각 문자의 최소 키 누름 횟수를 저장할 딕셔너리
-    min_press_count = {}
-    
-    # 각 키에 할당된 문자들을 순회하며 최소 키 누름 횟수 기록
-    for i, key in enumerate(keymap):
-        for j, char in enumerate(key):
-            if char not in min_press_count:
-                min_press_count[char] = j + 1  # j + 1이 키 누름 횟수
-            else:
-                min_press_count[char] = min(min_press_count[char], j + 1)
-
-    # 결과를 담을 리스트
+    #그냥 쉽게 2개의 자판에서 나오는 최솟값 저장
     result = []
-
-    # 각 목표 문자열을 작성하기 위해 필요한 최소 키 누름 횟수 계산
+    cnt = 0
     for target in targets:
-        total_presses = 0
-        for char in target:
-            if char in min_press_count:
-                total_presses += min_press_count[char]
-            else:
-                total_presses = -1
+        total = 0
+        ty = False
+        for i in range(len(target)):
+            min_val = 101
+            for key in keymap:
+                for t in range(len(key)):
+                    if key[t] == target[i]:
+                        min_val = min(min_val,t+1)
+                        break
+            if min_val == 101:
+                ty = True
+                result.append(-1)
                 break
-        result.append(total_presses)
-
+            total += min_val
+        if ty:
+            continue
+        result.append(total)
     return result
+                    
+                    
+        
+        
