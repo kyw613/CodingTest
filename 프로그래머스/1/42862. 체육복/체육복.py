cdcdf982cffc013cab52a16c7815d40bacbb9745
kyw613 <105292383+kyw613@users.noreply.github.com>
2024-12-23 19:22:25 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,15 +1,17 @@
 def solution(n, lost, reserve):
-    # 도난당했지만 여벌 체육복이 있는 학생들을 제거
-    lost_set = set(lost) - set(reserve)
-    reserve_set = set(reserve) - set(lost)
-    
-    for student in sorted(lost_set):
-        if student - 1 in reserve_set:
-            reserve_set.remove(student - 1)
-            lost_set.remove(student)
-        elif student + 1 in reserve_set:
-            reserve_set.remove(student + 1)
-            lost_set.remove(student)
-    
-    # 전체 학생 수에서 체육복을 빌리지 못한 학생 수를 뺀 값을 반환
-    return n - len(lost_set)
+    set_reserve = [r for r in reserve if r not in lost]
+    set_lost = [l for l in lost if l not in reserve]
+    set_lost.sort()
+    set_reserve.sort()
+
+    for i in range(len(set_reserve)):
+        if set_reserve[i] > 1:
+            if (set_reserve[i] -1) in set_lost:
+                set_lost.remove(set_reserve[i]-1)
+                continue
+        if set_reserve[i] < n:
+            if (set_reserve[i] + 1) in set_lost:
+                set_lost.remove(set_reserve[i]+1)
+    return n - len(set_lost)
+        
+                        
