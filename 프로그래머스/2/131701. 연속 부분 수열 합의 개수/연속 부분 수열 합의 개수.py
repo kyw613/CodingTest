19c8c1d5f60c9c2a0a8022d13ffd254e493d7a66
kyw613 <105292383+kyw613@users.noreply.github.com>
2024-12-30 15:08:33 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 278.14 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,14 +1,11 @@
 def solution(elements):
-    # 길게 추가해주면 되나? 
-    elements += elements
-    print(elements)
-    result = []
-    for k in range(len(elements) // 2):
-        # k+1은 사이즈 길이
-        total = 0
-        for i in range(0,len(elements) - (k+1)):
-            total = sum(elements[i:i+k+1])
-            result.append(total)
-    return len(set(result))
-        
-        
+    ll = len(elements)
+    res = set()
+
+    for i in range(ll):
+        ssum = elements[i]
+        res.add(ssum)
+        for j in range(i+1, i+ll):
+            ssum += elements[j%ll]
+            res.add(ssum)
+    return len(res)
