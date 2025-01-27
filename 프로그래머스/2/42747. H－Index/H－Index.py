7af2a0e194d798776752c6d0ef76038131516353
kyw613 <105292383+kyw613@users.noreply.github.com>
2025-01-27 22:27:32 +0900

    [level 2] Title: H-Index, Time: 0.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -2,5 +2,5 @@ def solution(citations):
     citations.sort(reverse=True)
     for i, k in enumerate(citations):
         if i + 1 > k:  # 논문의 순서 > 인용 수
-            return i
+            return i # 이 시점을 만족하는 값이 없음..
     return len(citations)  # 모든 논문이 조건을 만족
