be08e2a75e5870e772667daf961f1c9f7859b8aa
kyw613 <105292383+kyw613@users.noreply.github.com>
2025-03-01 13:18:46 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 160 ms, Memory: 117684 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,32 +1,28 @@
+N,K = map(int,input().split())
 from collections import deque
-
-N, K = map(int, input().split())
-
+# 1. DP 사용하기
 if N >= K:
-    print(N - K)
+    print(N-K)
     exit()
-
-MAX_SIZE = max(2 * K, 100000)
-DP = [1000001] * (MAX_SIZE + 1)
+max_size = max(2*K, 100000)
+DP = [1000001]* (max_size+1)
 DP[N] = 0
 order = deque([N])
-
-
 while order:
     val = order.popleft()
-    # *2 이동
-    if val * 2 <= MAX_SIZE and DP[val * 2] > DP[val]:
-        DP[val * 2] = DP[val]
-        order.appendleft(val * 2)
-
-    # +1 이동 
-    if val + 1 <= MAX_SIZE and DP[val + 1] > DP[val] + 1:
-        DP[val + 1] = DP[val] + 1
-        order.append(val + 1)
-
-    # -1 이동
-    if val - 1 >= 0 and DP[val - 1] > DP[val] + 1:
-        DP[val - 1] = DP[val] + 1
-        order.append(val - 1)
-
+    if val * 2 <=max_size:
+        a = DP[2*val]
+        DP[2*val] = min(DP[2*val],DP[val])
+        if a != DP[2*val]:
+            order.append(2*val)
+    if val + 1 <= max_size:
+        b = DP[val+1]
+        DP[val+1] = min(DP[val+1],DP[val]+1)
+        if b != DP[val+1]:
+            order.append(val+1)
+    if val - 1 >= 0:
+        c = DP[val-1]
+        DP[val-1] = min(DP[val-1],DP[val]+1)
+        if c != DP[val-1]:
+            order.append(val-1)
 print(DP[K])
